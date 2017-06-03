b4959070414658c1f0b42c5c1c5ea904ab43cc3b
Srinath Challa <challa.jobs@gmail.com>
2017-06-03 20:19:26 +0000

    ex-1

---
+++ pupppet-1/ntp_1.pp
@@ -0,0 +1,23 @@
+$ntp_conf = '# THis file is managed by Puppet
+Server 192.168.1.74 iburst
+driftfile /var/lib/ntp/drift
+'
+
+package { 'ntp':
+  ensure => present,
+}
+
+file { '/etc/ntp.conf':
+  ensure  => 'file',
+  content => $ntp_conf,
+  owner   => 'root',
+  group   => 'wheel',
+  mode    => '0660',
+}
+
+service { 'NTP_Service':
+  ensure => 'running',
+  enable => 'true',
+  name   => 'ntpd',
+}
+
